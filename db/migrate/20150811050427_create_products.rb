916d1a6360f5b7afd9f9ab107e7b9e37b95ea5b3
ahnita <ahn.bustamante@icloud.com>
2015-08-11 21:07:21 +0800

    amended db/migrate/20150811050427_create_products.rb

--- db/migrate/20150811050427_create_products.rb
+++ db/migrate/20150811050427_create_products.rb
@@ -1,13 +1,10 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.string :name
-      t.string :Johnny
-      t.string :English
-      t.string :description
-      t.string :Movie
-      t.string :1
-      t.movie1.jpg :image_url
+      t.string :movie
+      t.string :cinema
+      t.string :time
+      t.string :day
 
       t.timestamps null: false
     end
