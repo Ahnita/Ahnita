31067184d0e6a410c8b265e545fe28f62f725a49
ahnita <ahn.bustamante@icloud.com>
2015-08-12 17:25:40 +0800

    amended product stylesheet, products controller and application.html, products index.html and created database table for products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :Johnny, :English, :description, :Movie, :1, :image_url)
+      params.require(:product).permit(:movie, :cinema, :time, :day)
     end
 end
