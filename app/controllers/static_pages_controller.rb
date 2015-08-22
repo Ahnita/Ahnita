5552ce484edea1a50c76997c1ad2afe5df85e05c
ahnita <ahn.bustamante@icloud.com>
2015-08-22 22:36:06 +0800

    amended static_pages, static pages controller, created mailer basics, created contact_form and amended routes.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,18 @@ class StaticPagesController < ApplicationController
   end
 
   def landing_page
-  	@featured_product = Product.first
+  	@featured_product = Product.limit(3)
   end
+
+  def thank_you
+  	@name = params [:name]
+  	@email = params [:email]
+  	@message = params [:message]
+  	# ActionMailer::Base.mail(:from => @email,
+    #   :to => 'ahn.bustamante@gmail.com',
+    #   :subject => "A new contact form message from #{@name}",
+    #   :body => @message).deliver
+	UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
+  
 end
