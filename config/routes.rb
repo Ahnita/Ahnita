2aa00f19098ee43dc1c75544968da2df24194b71
ahnita <ahn.bustamante@icloud.com>
2015-08-31 06:26:18 +0300

    made changes in comments controller, added comment_params

--- config/routes.rb
+++ config/routes.rb
@@ -65,8 +65,10 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
+  resources :products do
+      resources :comments
+    end
+    resources :users
   # Example resource route with options:
   #   resources :products do
   #     member do
