30ee05be6b50ef79378bd193ada6681695a62d7a
ahnita <ahn.bustamante@icloud.com>
2015-09-08 10:02:10 +0800

    amended authentication password length.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :zxcvbnable
+         :recoverable, :rememberable, :trackable, :validatable, :zxcvbnable, password_length: 8..20
 
 
   has_many :orders
