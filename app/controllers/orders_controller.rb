f917e1804d44216ea857fd89b06744e8839a006c
ahnita <ahn.bustamante@icloud.com>
2015-09-15 14:45:45 +0800

    adding amendments to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,18 @@
 class OrdersController < ApplicationController
   def index
+    @products = Products.all
+    @cinema = Cinema.where("product_id = ?", Cinema.first.id)
+    @screening = Screening.where("cinema_id = ?", Screening.first.id)
+    @theater = Theater.where("screening_id = ?", Theater.first.id)
   end
   def order_params
   	  params.require(:order).permit(:user, :seat_number)
   end
   def show
+    @products = Product.find by("id =?", params[:product_id])
+    @cinema = Cinema.where("id =?", params[:cinema_id])
+    @screening = Screening.where("id =?", params[:screening_id])
+    @theater = Theater.where("id =?", params[:theater_id])
   end
 
   def new
