5da87facc83013bff63de663d0e41d4cbd740b38
ahnita <ahn.bustamante@icloud.com>
2015-09-20 19:42:42 +0800

    amendments in static pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,14 @@
 class StaticPagesController < ApplicationController
-  def booking
+   
+  def index 
+    @product = Product.all
+    @cinema = Cinema.where("product_id = ?", Product.first.id)
+    @screening = Screening.where("cinema_id = ?", Cinema.first.id)  
+    @theater = Theater.where("screeining_id = ?", Screening.first.id)
+  end
+
+  def show
+    @product = Product.find_by ("id = ?", params[:order][:product_id])
   end
 
   def landing_page
